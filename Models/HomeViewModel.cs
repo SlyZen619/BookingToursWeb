c4afccde23d8140a5e19774c688bcff27eea052a
Lê Tiến Đạt <datle2403@outlook.com>
2025-06-26 20:46:12 +0700

    Bổ sung model HomeView

---
+++ Models/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿// Models/HomeViewModel.cs
+using System.Collections.Generic;
+
+namespace BookingToursWeb.Models
+{
+    public class HomeViewModel
+    {
+        public List<Location> FamousPlaces { get; set; } = new List<Location>();
+        // Có thể thêm các thuộc tính khác ở đây nếu trang chủ cần hiển thị nhiều loại dữ liệu
+        // public List<Post> FeaturedPosts { get; set; }
+        // public List<Review> LatestReviews { get; set; }
+    }
+}
