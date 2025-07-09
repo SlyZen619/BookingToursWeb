c5c1814f7d30f8be5df12c1a03addab2039d3d76
Lê Tiến Đạt <datle2403@outlook.com>
2025-07-09 22:51:54 +0700

    Thêm cột vĩ độ và kinh độ cho bảng Locations

---
+++ Migrations/20250709073847_AddLatitudeLongitudeToLocations.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingToursWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLatitudeLongitudeToLocations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Latitude",
+                table: "Locations",
+                type: "decimal(9,6)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Longitude",
+                table: "Locations",
+                type: "decimal(9,6)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Locations");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Locations");
+        }
+    }
+}
